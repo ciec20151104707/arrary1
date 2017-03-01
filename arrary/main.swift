24d92c6469519e326f66f24ac21116f90b5527bf
liyi <s20151104707@mac24.local>
2017-03-01 11:18:45 +0800

    Initial Commit

---
+++ arrary/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  arrary
+//
+//  Created by s20151104707 on 17/3/1.
+//  Copyright © 2017年 s20151104707. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
